a59a329092f48113c3fedd3ffedb45afd5a74ba1
lenovo <lenovo@DESKTOP-R8R825I>
2022-02-11 13:50:04 +0530

    By Anant1

--- DemoGitTb13/src/Demo.java
+++ DemoGitTb13/src/Demo.java
@@ -2,6 +2,15 @@
 public class Demo 
 {
 	
+	public void m1()
+	{
+		System.out.println("Helllo M1");
+	}
+	
+	
+	
+	
+	
 	public static void main(String[] args)
 	{
 		System.out.println("Hello Main of Git");
